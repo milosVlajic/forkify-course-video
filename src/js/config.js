33ce1fd206b30822378545a2385440f6048ad451
milosVlajic <mvlajic90@gmail.com>
2021-03-23 13:23:04 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c41c95e7-02e3-4544-81ca-36903b8dba4f';
+export const MODAL_CLOSE_SEC = 2.5;
